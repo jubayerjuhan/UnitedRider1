c5452b907ae605a60a72f56fe771d04a2cf4cff5
jubayerjuhan <davidjuhan23@gmail.com>
2021-08-08 16:57:38 +0600

    Working On Signup Page

--- src/Components/SignUp/SignForm.js
+++ src/Components/SignUp/SignForm.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import './SignForm.css'
 
 const SignForm = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
 
-    console.log(watch("example")); // watch input value by passing the name of it
+    // console.log(watch("example")); // watch input value by passing the name of it
 
     return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register("example")} />
+        <form className="signup-form" onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="SignupTitle">Sign Up</h2>
 
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("exampleRequired", { required: true })} />
+            <input className="input" {...register("name", { required: true, pattern: /^[A-Za-z]+$/i })} placeholder="Full Name"/>
+            {errors.name && <span>Use Charecters Only For Your Name</span>}
+
+            <input className="input" {...register("email", { required: true, pattern: /\S+@\S+\.\S+/i })} placeholder="Email Address"/>
+            {errors.email && <span>Please Check Your Email Address</span>}
+
+            <input className="input" {...register("password", { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i })} placeholder="Password"/>
+            {errors.password && <span>Pass Must Contain Minimum 8 characters, at least one Number</span>}
+
+            <input className="input" {...register("checkPass", { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i })} placeholder="Confirm Address"/>
+            {errors.checkPass && <span>Pass Must Contain Minimum 8 characters, at least one Number</span>}
+
+
             {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
 
-            <input type="submit" />
+            <input style={{border: 'none'}} className="input" id="btn" type="submit" />
         </form>
     );
 };
