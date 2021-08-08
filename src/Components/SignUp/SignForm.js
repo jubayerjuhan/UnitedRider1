85dbb8aeec3394a81f2f9ecb0c9154d951d78773
jubayerjuhan <davidjuhan23@gmail.com>
2021-08-08 17:28:19 +0600

    Google Btn Added

--- src/Components/SignUp/SignForm.js
+++ src/Components/SignUp/SignForm.js
@@ -21,7 +21,7 @@ const SignForm = () => {
             <input className="input" {...register("password", { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i })} placeholder="Password"/>
             {errors.password && <span>Pass Must Contain Minimum 8 characters, at least one Number</span>}
 
-            <input className="input" {...register("checkPass", { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i })} placeholder="Confirm Address"/>
+            <input className="input" {...register("checkPass", { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i })} placeholder="Confirm Password"/>
             {errors.checkPass && <span>Pass Must Contain Minimum 8 characters, at least one Number</span>}
 
 
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Signup.css'
 import SignForm from "./SignForm";
+import GoogleButton from 'react-google-button'
+
+
 const SignUp = () => {
     return (
         <div className="signUpContainer">
             <SignForm></SignForm>
-            <button>SignUp With Google</button>
+            <button className="Google-Btn" id="button"><i class="fab fa-google fa-2x"></i> Sign Up With Google</button>
+            {/* <button className="Facebook-Btn">SignUp With Facebook</button> */}
+            <button className="Google-Btn" id="button"><i class="fab fa-google fa-2x"></i> Sign Up With Google</button>
+            <GoogleButton/>
         </div>
     );
 };
